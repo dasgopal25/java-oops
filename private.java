56047088c5c5a097193ff93f2bc0c58b2fd7392f
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2025-09-18 21:00:02 +0530

    add

---
+++ private.java
@@ -0,0 +1,12 @@
+class A{
+    int a; double b; String c;
+    private A(){
+        a=10;
+        b = 20.00;
+        c = "OOPS";
+    }
+
+     void show(){
+        System.out.println(a+" "+b+" "+c);
+    }
+}
